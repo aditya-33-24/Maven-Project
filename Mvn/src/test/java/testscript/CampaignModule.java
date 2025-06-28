43fa1a5b74485a9b9eb62b9072d204f2f316c57d
User <User@Aditya>
2025-06-28 12:14:23 +0530

    Added open browser

--- Mvn/src/test/java/testscript/CampaignModule.java
+++ Mvn/src/test/java/testscript/CampaignModule.java
@@ -1,11 +1,17 @@
 package testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class CampaignModule {
 	@Test
 public void createCampaign() {
-	Reporter.log("Campaign has been created",true);
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.fb.com");
+	Reporter.log(driver.getTitle(),true);
+//	Reporter.log("Campaign has been created",true);
+	driver.quit();
 }
 }
