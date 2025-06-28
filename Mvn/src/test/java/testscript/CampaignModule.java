34a10a8d5924a1f438d853b4f4ca701902d3be08
User <User@Aditya>
2025-06-28 11:20:47 +0530

    MvnPrjoect

---
+++ Mvn/src/test/java/testscript/CampaignModule.java
@@ -0,0 +1,11 @@
+package testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CampaignModule {
+	@Test
+public void createCampaign() {
+	Reporter.log("Campaign has been created",true);
+}
+}
